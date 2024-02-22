cf4bdc05ac81f866a3fabe9d13eccdc843a7433b
pratikb2001 <pratikbodhane@gmail.com>
2024-02-22 14:46:05 +0530

    code added for data fetching from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
@@ -21,6 +23,7 @@ import {
 } from "@/components/ui/table";
 import { Checkbox } from "@/components/ui/checkbox";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import axios from "axios";
 
 
 
@@ -30,6 +33,32 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 
 export default function Home() {
+
+  const [applicants, setApplicants] = useState([]);
+
+  useEffect(() => {
+
+    try {
+      
+      const myFunction = async() => {
+        const response = await axios.get('https://dummyjson.com/products');
+        console.log(response.data.products[0].title)
+        setApplicants(response.data.products)
+      
+      }
+
+      myFunction()
+
+    } catch (error) {
+      console.error(error)
+    }
+
+  }, []);
+
+  
+
+  console.log(applicants)
+
   return (
     <div>
       <div className="flex justify-between">
@@ -53,6 +82,7 @@ export default function Home() {
       </div>
 
       <button className=""></button>
+
       <Tabs defaultValue="account" className="w-full mb-8 ">
         <TabsList className="bg-white">
           <TabsTrigger value="Overview">Overview</TabsTrigger>
@@ -148,159 +178,33 @@ export default function Home() {
                 <TableHead>Last Salary</TableHead>
                 <TableHead>Phone Number</TableHead>
                 <TableHead>Pin Code</TableHead>
-                <TableHead>Date & Time</TableHead>
+                {/* <TableHead>Date & Time</TableHead> */}
               </TableRow>
             </TableHeader>
             <TableBody>
 
-              <TableRow>
-                <TableCell className="font-medium">
-                  <Checkbox />
-                </TableCell>
-                <TableCell className="font-medium">1</TableCell>
-                <TableCell className="font-medium">Balamurugan</TableCell>
-                <TableCell className="font-medium">
-                  <RadioGroup defaultValue="option-one">
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem value="option-one" id="option-one" />40%
-                    </div>
-                  </RadioGroup>
-                </TableCell>
-                <TableCell>15000</TableCell>
-                <TableCell>2742090989</TableCell>
-                <TableCell>2832893</TableCell>
-                <TableCell>{`27 Sep'22 11:15AM`}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="font-medium">
-                  <Checkbox />
-                </TableCell>
-                <TableCell className="font-medium">2</TableCell>
-                <TableCell className="font-medium">Shugumar</TableCell>
-                <TableCell className="font-medium">
-                  <RadioGroup defaultValue="option-one">
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem value="option-one" id="option-one" />70%
-                    </div>
-                  </RadioGroup>
-                </TableCell>
-                <TableCell>-</TableCell>
-                <TableCell>2742090989</TableCell>
-                <TableCell>2832893</TableCell>
-                <TableCell>{`27 Sep'22 11:15AM`}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="font-medium">
-                  <Checkbox />
-                </TableCell>
-                <TableCell className="font-medium">3</TableCell>
-                <TableCell className="font-medium">VijayKumar</TableCell>
-                <TableCell className="font-medium">
-                  <RadioGroup defaultValue="option-one">
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem value="option-one" id="option-one" />70%
-                    </div>
-                  </RadioGroup>
-                </TableCell>
-                <TableCell>12500</TableCell>
-                <TableCell>2742090989</TableCell>
-                <TableCell>2832893</TableCell>
-                <TableCell>{`27 Sep'22 11:15AM`}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="font-medium">
-                  <Checkbox />
-                </TableCell>
-                <TableCell className="font-medium">4</TableCell>
-                <TableCell className="font-medium">AjayKumar.S</TableCell>
-                <TableCell className="font-medium">
-                  <RadioGroup defaultValue="option-one">
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem value="option-one" id="option-one" />10%
-                    </div>
-                  </RadioGroup>
-                </TableCell>
-                <TableCell>5000</TableCell>
-                <TableCell>2742090989</TableCell>
-                <TableCell>2832893</TableCell>
-                <TableCell>{`27 Sep'22 11:15AM`}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="font-medium">
-                  <Checkbox />
-                </TableCell>
-                <TableCell className="font-medium">5</TableCell>
-                <TableCell className="font-medium">V.Dhawan</TableCell>
-                <TableCell className="font-medium">
-                  <RadioGroup defaultValue="option-one">
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem value="option-one" id="option-one" />70%
-                    </div>
-                  </RadioGroup>
-                </TableCell>
-                <TableCell>-</TableCell>
-                <TableCell>2742090989</TableCell>
-                <TableCell>2832893</TableCell>
-                <TableCell>{`27 Sep'22 11:15AM`}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="font-medium">
-                  <Checkbox />
-                </TableCell>
-                <TableCell className="font-medium">6</TableCell>
-                <TableCell className="font-medium">Guhan M</TableCell>
-                <TableCell className="font-medium">
-                  <RadioGroup defaultValue="option-one">
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem value="option-one" id="option-one" />40%
-                    </div>
-                  </RadioGroup>
-                </TableCell>
-                <TableCell>30,000</TableCell>
-                <TableCell>2742090989</TableCell>
-                <TableCell>2832893</TableCell>
-                <TableCell>{`27 Sep'22 11:15AM`}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="font-medium">
-                  <Checkbox />
-                </TableCell>
-                <TableCell className="font-medium">7</TableCell>
-                <TableCell className="font-medium">Mohanraj S</TableCell>
-                <TableCell className="font-medium">
-                  <RadioGroup defaultValue="option-one">
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem value="option-one" id="option-one" />30%
-                    </div>
-                  </RadioGroup>
-                </TableCell>
-                <TableCell>14,500</TableCell>
-                <TableCell>2742090989</TableCell>
-                <TableCell>2832893</TableCell>
-                <TableCell>{`27 Sep'22 11:15AM`}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="font-medium">
-                  <Checkbox />
-                </TableCell>
-                <TableCell className="font-medium">8</TableCell>
-                <TableCell className="font-medium">Thirumuguram</TableCell>
-                <TableCell className="font-medium">
-                  <RadioGroup defaultValue="option-one">
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem value="option-one" id="option-one" />3  0%
-                    </div>
-                  </RadioGroup>
-                </TableCell>
-                <TableCell>12,500</TableCell>
-                <TableCell>2742090989</TableCell>
-                <TableCell>2832893</TableCell>
-                <TableCell>{`27 Sep'22 11:15AM`}</TableCell>
-              </TableRow>
+
+              {applicants.map(applicant => (
+                <TableRow key={applicant.id}>
+                  <TableCell>{applicant.title}</TableCell>
+                  <TableCell>{applicant.price}</TableCell>
+                  <TableCell>{applicant.category}</TableCell>
+                  <TableCell>{applicant.discountPercentage}</TableCell>
+                  <TableCell>{applicant.rating}</TableCell>
+                  <TableCell>{applicant.stock}</TableCell>
+                  <TableCell>{applicant.brand}</TableCell>
+
+                </TableRow>
+              ))}
+
+
+
+              
 
             </TableBody>
           </Table>
         </TabsContent>
+
       </Tabs>
     </div>
   );
